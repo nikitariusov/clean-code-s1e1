e700a25c7e35ce8e41d9b0a9477c86170e8a51a6
Nikita Usov <nikitariusov@gmail.com>
2022-10-31 00:09:36 +0200

    refactor: alt для мультимедиа

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button button_delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="button__img"
+    deleteButtonImg.alt=""
     deleteButton.appendChild(deleteButtonImg);
 
 
